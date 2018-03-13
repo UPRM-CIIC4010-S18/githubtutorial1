c1c62bb23897120cb5fe913d7c995e4a1b026fe4
My Computer <My Computer@My-PC>
2018-03-13 12:12:32 -0400

    Initial chekin sales tools

---
+++ SalesTools/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesTools data = new SalesTools();
+		
+		displayGreeting();
+		data.display();
+		
+		}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello Happy Sales People");
+		System.out.println("This app shows sales data");
+		System.out.println("--------------------------");
+
+
+		System.out.println("Test 1");
+	}
+
+}
